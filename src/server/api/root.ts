405526cd2d10e5228930fff9de9f25e65f8bf9f4
ervin <employervin@gmail.com>
2024-11-14 20:34:52 -0500

    Add trpc memories router, strip UI

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "@/server/api/routers/post";
+import { memoriesRouter } from "@/server/api/routers/memories";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  memories: memoriesRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/memories.ts
+++ src/server/api/routers/memories.ts
@@ -2,10 +2,11 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
+export const memoriesRouter = createTRPCRouter({
   getAll: publicProcedure
     .input(
       z.object({
+        username: z.string().optional(),
         orderBy: z.enum(["asc", "desc"]).default("desc"),
       }),
     )
@@ -15,9 +16,102 @@ export const postRouter = createTRPCRouter({
       }
 
       const memories = await ctx.db.memories.findMany({
-        orderBy: { timeStamp: input.orderBy },
+        orderBy: { timestamp: input.orderBy },
+        where: {
+          username: input.username,
+        },
       });
 
       return memories ?? [];
+    }),
+  createOne: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string().optional(),
+        timestamp: z.string().optional(),
+        username: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.db.memories) {
+        throw new Error("Database connection error");
+      }
+
+      const memory = await ctx.db.memories.create({
+        data: {
+          name: input.name,
+          description: input.description,
+          timestamp: input.timestamp,
+          username: input.username,
+        },
+      });
+
+      return memory;
+    }),
+  getOne: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      if (!ctx.db.memories) {
+        throw new Error("Database connection error");
+      }
+
+      const memory = await ctx.db.memories.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return memory;
+    }),
+  updateOne: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string().optional(),
+        description: z.string().optional(),
+        timestamp: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.db.memories) {
+        throw new Error("Database connection error");
+      }
+
+      const memory = await ctx.db.memories.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+          timestamp: input.timestamp,
+        },
+      });
+
+      return memory;
+    }),
+  deleteOne: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.db.memories) {
+        throw new Error("Database connection error");
+      }
+
+      const memory = await ctx.db.memories.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return memory;
     }),
 });
