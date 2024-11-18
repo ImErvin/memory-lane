d13b1c0b2226f85aca512b71e1e90208573773ab
ervin <employervin@gmail.com>
2024-11-18 10:54:57 -0500

    Fix add missing import

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { upload } from "@vercel/blob/client";
 import { clsx, type ClassValue } from "clsx";
+import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
